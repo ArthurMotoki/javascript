52c6057cf034b48e479f3b0ac87f4692a90211a4
ArthurMotoki <101857914+ArthurMotoki@users.noreply.github.com>
2022-07-25 23:17:06 +0900

    novo

---
+++ desafios/Aula 13/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+    var msg  = document.getElementById('msg')
+    var img  = document.getElementById('imagem')
+    var data = new Date()   
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!!!
+        img.src = 'img/manha.png'
+        document.body.style.background = '#9beeff'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'img/tarde.png'
+        document.body.style.background = '#fca432'
+        //BOA TARDE!!!    
+    } else {
+        img.src = 'img/noite.png'
+        document.body.style.background = '#183147'
+        //BOA NOITE
+    }
+}
+
+
+
---
+++ exercicios/erika.js
@@ -0,0 +1,33 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+console.log(`Hoje é dia de:`)
+
+switch(diaSem) {
+    case 0:
+        console.log('Dar o bumbum')
+        break
+    case 1: 
+        console.log('Sexo')
+        break
+    case 2:
+        console.log('Anal')
+        break
+    case 3: 
+        console.log('Mamada')
+        break
+    case 4:
+        console.log('putaria braba')
+        break
+    case 5: 
+        console.log('sexo na montanha')
+        break
+    case 6: 
+        console.log('Estudar')
+        break
+    default:
+        console.log('[Erro] Dia invalido')
+        break
+}
+
+
