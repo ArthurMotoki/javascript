3f531a18bc904dfcf8439c5a1d10b71c0908cc89
ArthurMotoki <101857914+ArthurMotoki@users.noreply.github.com>
2022-07-26 13:11:19 +0900

    new

--- desafios/erika001/script.js
+++ desafios/erika001/script.js
@@ -1,5 +1,34 @@
-var msg  = document.getElementById('msg')
-var img  = document.getElementById('img')
-var data = new Date()
-var hora = data.getDay
-var min  = data.getMinutes
+function  base() {
+    var msg  = document.getElementById('msg')
+    var img  = document.getElementById('img')
+    var dtn  = document.getElementById('dtn')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 19
+    var min  = data.getMinutes()
+    msg.innerHTML = `Agora são exatos ${hora}:${min}!`
+    if (hora >= 5 && hora <= 12) {
+        //Dia
+        img.src = 'img/manha-(1).jpg'
+        document.body.style.backgroundColor = '#68C2FF'
+        dtn.innerHTML = 'Bom dia, Flor do dia!'
+    } else if (hora >= 13 && hora <= 18) {
+        //Tarde
+        img.src = 'img/tarde.png'
+        document.body.style.backgroundColor = '#AA7CA2'
+        dtn.innerHTML = 'Boa tarde, minha Princesa!'
+    } else if (hora >= 19 && hora <= 23) {
+        //Noite
+        img.src = 'img/noite.jpg'
+        document.body.style.backgroundColor = '#18495C'
+        dtn.innerHTML = 'Boa noite, meu doce de coco!'
+    } else {
+        //Madru
+        img.src = 'img/madru.jpg'
+        document.body.style.backgroundColor = '#ffbfd9'
+        dtn.innerHTML = 'Vai dormir! Boa noite meu amor.'
+    }
+
+}
+
+
