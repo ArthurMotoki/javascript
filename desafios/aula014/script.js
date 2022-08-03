f55a024707151c2591b62d767d3ec9aa4bc8fe85
ArthurMotoki <101857914+ArthurMotoki@users.noreply.github.com>
2022-08-03 22:46:06 +0900

    novudade nova

--- desafios/aula014/script.js
+++ desafios/aula014/script.js
@@ -16,7 +16,7 @@ function verificar() {
             if (idade >= 0 && idade < 10 ) {
                 //Criança
                 img.setAttribute('src','imagens/kid-m.png')
-            } else if (idade < 21) {
+            } else if (idade < 24) {
                 //Jovem
                 img.setAttribute('src','imagens/jovem-m.png')
             } else if (idade < 60) {
